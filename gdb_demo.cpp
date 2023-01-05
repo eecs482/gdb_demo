17e3ba20eadfca77524d6fe0860be71109243e89
Steven Xia <97299316+xia-steven@users.noreply.github.com>
2023-01-05 17:10:36 -0500

    Update gdb_demo.cpp

--- gdb_demo.cpp
+++ gdb_demo.cpp
@@ -1,8 +1,14 @@
 // Note: this demonstration should be done on CAEN, it has produced undefined results on other platforms.
 
-int foo(int* array, int size) {
+#include <iostream>
+
+using namespace std;
+
+// Requires: array contains non-zero integers
+// Effects: calculate the sum of the integer inverses of an array times ten
+int foo(const int *array, unsigned int size) {
 	int sum = 0;
-	for (int i = 0; i < size; i++) {
+	for (unsigned int i = 0; i < size; i++) {
 		int elt = 10 / array[i];
 		sum += elt;
 	}
@@ -10,7 +16,8 @@ int foo(int* array, int size) {
 }
 
 int main() {
-	int arr[3] = {2, 1, 0};
-	int x = 3;
-	int result = foo(arr, x);
+	int arr[5] = {2, 1, 0};
+	unsigned int arr_size = 3;
+	int result = foo(arr, arr_size);
+	cout << result << endl;
 }
